70b19744d2821e471687193d81929f7b85b178f5
Ahmed Mohamed Essmat <49377210+ahmedessmat5254@users.noreply.github.com>
2021-06-12 22:14:22 +0200

    Add a Search bar and table view. The Search put fake result in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Ahmed Essmat on 12/06/2021.
+//
+
+import UIKit
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
